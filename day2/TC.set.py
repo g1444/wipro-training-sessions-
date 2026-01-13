b636f1b5633c50a1db7e0728d6078d8b6753d302
g1444 <varidireddy.gowtham.reddy.1444@gmail.com>
2026-01-13 12:59:12 +0530

    changes staged

---
+++ day2/TC.set.py
@@ -0,0 +1,18 @@
+se={1,2,3,6,3,4,45,5,6,54,54,5}
+
+print(se)
+for i in se:
+    print(i)
+se.add(10204)
+print(se)
+
+f={1,2,3,4,4,5,6,6}
+g={3,4,5,56,6,7,87,8}
+
+# set functions
+# set union
+print(f|g)
+# set intersection
+print(f&g)
+
+print(2 in f)
