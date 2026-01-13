ef79ad963d7892f80f8f27bc77694cf78f14a02f
g1444 <varidireddy.gowtham.reddy.1444@gmail.com>
2026-01-13 11:32:48 +0530

    changes made

--- day2/TC_inbuilt.py
+++ day2/TC_inbuilt.py
@@ -2,3 +2,10 @@ import math as m
 print(m.sqrt(225))
 print(m.log(200))
 print(m.gamma(100))
+
+a=20
+b=33
+if a==b :
+    print(True)
+else:
+    print(False)
---
+++ day2/TC_string.py
@@ -0,0 +1,23 @@
+s="helo world"
+t="hello everyone"
+# printing a certain char ina string with index
+print(s[2],s[3],s[-3],s[1])
+
+print(s)
+# find a sub string in a string 
+print(s.find("el"))
+# string spilit in to list
+print(s.split())
+# string slicing 
+print(s[3:-1])
+# string spiliting 
+print(s[3:-1:2])
+# string 
+print(s.upper())
+print(s.replace("hel","heaven"))
+print(s+t)
+print(len(s))
+
+name=input("enter name")
+age=input("enter age")
+print("hello my name is {0} im {1} years old ".format(name,age))
